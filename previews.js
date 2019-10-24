413b001401342c3377a2b8479f999dac1d379fc2
Bartłomiej Sacharski <beton@cementownia.org>
2019-10-24 21:28:48 +0200

    Initial commit

---
+++ previews.js
@@ -0,0 +1,14 @@
+(function() {
+  const mealData = {
+    meals: {
+      "zapiekanka makaronowa z warzywami w sosie beszamelowym": [
+        {
+          fullImage: "images/7fa1055cfe2fb7b4f40885bc2c176fc3e1869c7e.jpg",
+          thumb: "images/7fa1055cfe2fb7b4f40885bc2c176fc3e1869c7e_thumb.jpg"
+        }
+      ]
+    }
+  };
+
+  window.mealData = mealData;
+})();
