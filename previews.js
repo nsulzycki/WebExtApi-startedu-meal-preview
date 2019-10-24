dbcdda824fe7209eafca44b9293089e01ba0701d
Bartłomiej Sacharski <beton@cementownia.org>
2019-10-24 22:47:12 +0200

    Added two more meals

--- previews.js
+++ previews.js
@@ -7,10 +7,22 @@
           thumb: "images/IMG_3658_thumb.jpg"
         }
       ],
+      "kaszotto gryczane z kurczakiem, burakiem i porem": [
+        {
+          fullImage: "images/IMG-0794.JPG",
+          thumb: "images/IMG-0794_thumb.JPG"
+        }
+      ],
       "klopsiki w sosie pomidorowo-koperkowym, kasza gryczana, buraczki na ciepło": [
         {
           fullImage: "images/IMG_3652.jpg",
           thumb: "images/IMG_3652_thumb.jpg"
+        }
+      ],
+      "kotlety ziemniaczane w sosie pieczarkowym, jajko na twardo, coleslaw": [
+        {
+          fullImage: "images/IMG-0793.JPG",
+          thumb: "images/IMG-0793_thumb.JPG"
         }
       ],
       "zapiekanka makaronowa z warzywami w sosie beszamelowym": [
