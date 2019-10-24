d5ea1de7d11f354431f55d52615e3b9bea01374e
Bartłomiej Sacharski <beton@cementownia.org>
2019-10-24 21:52:29 +0200

    Initial comit to GH pages

--- previews.js
+++ previews.js
@@ -1,10 +1,28 @@
 (function() {
   const mealData = {
     meals: {
+      "butter chicken, kasza perłowa z pietruszką, surówka szwedzka": [
+        {
+          fullImage: "images/IMG_3658.jpg",
+          thumb: "images/IMG_3658_thumb.jpg"
+        }
+      ],
+      "klopsiki w sosie pomidorowo-koperkowym, kasza gryczana, buraczki na ciepło": [
+        {
+          fullImage: "images/IMG_3652.jpg",
+          thumb: "images/IMG_3652_thumb.jpg"
+        }
+      ],
       "zapiekanka makaronowa z warzywami w sosie beszamelowym": [
         {
           fullImage: "images/7fa1055cfe2fb7b4f40885bc2c176fc3e1869c7e.jpg",
           thumb: "images/7fa1055cfe2fb7b4f40885bc2c176fc3e1869c7e_thumb.jpg"
+        }
+      ],
+      żurek: [
+        {
+          fullImage: "images/IMG_3656.jpg",
+          thumb: "images/IMG_3656_thumb.jpg"
         }
       ]
     }
